dcc62e8fd628757ba07dc6a74a38c8d787abfab6
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-04-16 00:28:45 -0500

    feature: added routing to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: TestComponent,
+        title: 'Test'
+    }, {
+        path: 'home',
+        component: HomeComponent,
+        title: 'Home'
+    }
+];
