3f10c811969a5d6c182c620291b1a7770975e740
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-04-16 02:00:49 -0500

    feature: added footer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { TestComponent } from './modules/test/test.component';
 import { HomeComponent } from './modules/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HeaderComponent,
+        component: FooterComponent,
         title: 'Test'
     }, {
         path: 'home',
