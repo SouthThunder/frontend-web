817aff30b48e3a01557671c4ce97e46d7feed111
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-04-24 00:38:02 -0500

    update: credit card styling

--- src/app/pago/pago.component.ts
+++ src/app/pago/pago.component.ts
@@ -22,17 +22,36 @@ export class PagoComponent {
   cardMonth: string = '';
   cardYear: string = '';
   formattedCardNumber: string = '';
+  cardType: string = 'Unknown';
+  cardImageUrl: string = './assets/default.png';
 
   onSubmit() {
-    console.log('Card Data:', this.cardNumber, this.cardName, this.cardMonth, this.cardYear);
+    console.log('Card Data:', this.cardNumber, this.cardName, this.cardMonth, this.cardYear, this.cardType);
   }
 
   onCardNumberChange(value: string) {
     this.cardNumber = value.replace(/\s+/g, ''); // Remove any whitespace
     this.formattedCardNumber = this.formatCardNumber(this.cardNumber);
+    this.detectCardType(this.cardNumber);
   }
 
   formatCardNumber(cardNumber: string): string {
     return cardNumber.replace(/(\d{4})(?=\d)/g, '$1 '); // Adds a space after every 4 digits
+  }
+
+  detectCardType(cardNumber: string): void {
+    if (cardNumber.startsWith('4')) {
+      this.cardType = 'Visa';
+      this.cardImageUrl = './assets/visa.png';
+    } else if (/^5[1-5]/.test(cardNumber)) {
+      this.cardType = 'MasterCard';
+      this.cardImageUrl = './assets/mastercard.png';
+    } else if (/^3[47]/.test(cardNumber)) {
+      this.cardType = 'American Express';
+      this.cardImageUrl = './assets/amex.png';
+    } else {
+      this.cardType = 'Unknown';
+      this.cardImageUrl = './assets/default.png';
+    }
   }
 }
