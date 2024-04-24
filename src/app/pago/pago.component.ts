5fe0ca88abebe6d1425fda765031bf6e57730466
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-04-24 00:02:53 -0500

    feature: added pago component

--- src/app/pago/pago.component.ts
+++ src/app/pago/pago.component.ts
@@ -3,13 +3,36 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { FooterComponent } from '../components/footer/footer.component';
 
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
+
+
+
 @Component({
   selector: 'app-pago',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './pago.component.html',
   styleUrl: './pago.component.css'
 })
 export class PagoComponent {
+  cardNumber: string = '';
+  cardName: string = '';
+  cardMonth: string = '';
+  cardYear: string = '';
+  formattedCardNumber: string = '';
 
+  onSubmit() {
+    console.log('Card Data:', this.cardNumber, this.cardName, this.cardMonth, this.cardYear);
+  }
+
+  onCardNumberChange(value: string) {
+    this.cardNumber = value.replace(/\s+/g, ''); // Remove any whitespace
+    this.formattedCardNumber = this.formatCardNumber(this.cardNumber);
+  }
+
+  formatCardNumber(cardNumber: string): string {
+    return cardNumber.replace(/(\d{4})(?=\d)/g, '$1 '); // Adds a space after every 4 digits
+  }
 }
