2e1c4195029d22b3d3a13ed6026effccd240f785
JoseRodriguez <93455373+Kose117@users.noreply.github.com>
2024-05-26 21:25:17 -0500

    feature: mejoras css y toast

--- src/app/modules/properties-catalog/properties-catalog.component.ts
+++ src/app/modules/properties-catalog/properties-catalog.component.ts
@@ -3,7 +3,7 @@ import { FooterComponent } from "../../components/footer/footer.component";
 import { HeaderComponent } from "../../components/header/header.component";
 import { PropertieComponent } from "../../propertie/propertie.component";
 import { Propiedad } from '../../models/propiedadmode';
-
+import { SweetAlertService } from '../../services/sweetAlertService/sweet-alert.service';
 import { PropertiesService } from '../../services/propiedad.service/properties.service';
 import { NgFor } from '@angular/common';
 
@@ -19,9 +19,12 @@ export class PropertiesCatalogComponent {
 
     propiedades: Propiedad[] = []; 
 
-    constructor() {
+    constructor(private sweetAlertService: SweetAlertService) {
         this.ngOnInit()
     }
+    mostrarAlerta() {
+        this.sweetAlertService.mostrarCorrectamente('Propiedades encontradas');
+      }
 
     async ngOnInit() {
         const propertiesService = new PropertiesService();
