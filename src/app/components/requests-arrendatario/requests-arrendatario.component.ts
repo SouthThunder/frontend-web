20c6ef6e7c2b9b50ac41df55cef350e4495737a6
CarlosLopez <carlosgablopezm@gmail.com>
2024-05-28 18:24:34 -0500

    nose

--- src/app/components/requests-arrendatario/requests-arrendatario.component.ts
+++ src/app/components/requests-arrendatario/requests-arrendatario.component.ts
@@ -1,30 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { SolicitudArriendo } from '../../models/solicitudmodel';
 import { SolicitudService } from '../../services/solicitudService/solicitud.service';
+import Cookies from 'js-cookie';
 
 @Component({
   selector: 'app-requests-arrendatario',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './requests-arrendatario.component.html',
-  styleUrl: './requests-arrendatario.component.css'
+  styleUrls: ['./requests-arrendatario.component.css']
 })
 export class RequestsArrendatarioComponent {
   solicitudes: SolicitudArriendo[] = [];
-  constructor(private solicitudService:SolicitudService) { }
+
+  constructor(private solicitudService: SolicitudService) { }
+
   ngOnInit(): void {
-    // this.loadRequests();
+    this.loadRequests();
   }
-  // async loadRequests(): Promise<void> {
-  //   (await this.solicitudService.getRequests()).subscribe(
-  //     (response) => {
-  //       this.solicitudes = response || [];
-  //     },
-  //     (error) => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }
 
-
+  async loadRequests() {
+    try {
+      const token = Cookies.get('token');
+      if (token) {
+        const response = await this.solicitudService.getSolicitudesByArrendador(token);
+        this.solicitudes = response;
+        console.log(this.solicitudes);
+      } else {
+        console.error('Token is undefined');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
