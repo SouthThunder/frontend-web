05ae127dc236dcf8fdb963defb1235cb1c166dfa
Luis Alejandro Vera Moreno <98617444+SouthThunder@users.noreply.github.com>
2024-05-28 00:09:47 -0500

    feature: added profile info service

--- src/app/modules/profile/profile.component.ts
+++ src/app/modules/profile/profile.component.ts
@@ -5,6 +5,9 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { RequestsArrendadorComponent } from '../../components/requests-arrendador/requests-arrendador.component';
 import { CommonModule } from '@angular/common';
 import { FormPropertyComponent } from '../../components/form-property/form-property.component';
+import { ArrendadorService } from '../../services/arrendadorService/arrendador.service';
+import Cookies from 'js-cookie';
+import { Arrendador } from '../../models/arrendadormodel';
 
 @Component({
   selector: 'app-profile',
@@ -14,11 +17,25 @@ import { FormPropertyComponent } from '../../components/form-property/form-prope
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  arrendador: Arrendador | null = null;
 
+  constructor(private router: Router, private route: ActivatedRoute, private arrendadorService : ArrendadorService) { 
+    this.getInfo();
+  }
 
   async getInfo() {
-    
+    try {
+      const token = Cookies.get('token');
+      if (token) {
+        const arrendador = await this.arrendadorService.authArrendador(token);
+        if (arrendador) {
+          console.log(arrendador)
+          this.arrendador = arrendador;
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   mostrarPropertiesArrendador = true;
